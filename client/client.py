89b466c7bba7ed3dd743b3496ef57e99489ce058
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-06-19 16:52:22 +0200

    added put functionality

--- client/client.py
+++ client/client.py
@@ -1,6 +1,8 @@
 from socket import *
 import os
 import sys
+
+comands = ["list","get","put","help"]
 path = os.path.dirname(__file__)+"\\download\\" #if the folder doesn't exist
 if(not os.path.exists(path)):
     os.mkdir(path)
@@ -11,14 +13,13 @@ clientsocket = socket(AF_INET, SOCK_DGRAM)
 
 while True:
     message = input('input the command: ')
+    clientsocket.sendto(message.encode(),(servername,serverport))
     
-    if(message=='ls'):
-        clientsocket.sendto("ls".encode(),(servername,serverport))
+    if(message==comands[0]): #list comand
         s_answer, s_address = clientsocket.recvfrom(2048)
         print(s_answer.decode())
    
-    if(message.__contains__('download')):
-        clientsocket.sendto(message.encode(),(servername,serverport))
+    if(message.__contains__(comands[1])):#get comand
         title, serveraddress = clientsocket.recvfrom(2048)
         file = open(path+title.decode(),'wb')
         while True:
@@ -28,9 +29,24 @@ while True:
                 break
             else:
                 file.write(packet)
-                
+    if(message.__contains__(comands[2])):#put comand
+        title = message.split()[1]
+        clientsocket.sendto(title.encode(),(servername,serverport))
+        file = open(path+title ,'rb')
+        while True:
+            packet = file.read()
+            if(packet==b''):
+                clientsocket.sendto(packet,(servername,serverport))
+                file.close()
+                break
+            else:
+                clientsocket.sendto(packet,(servername,serverport))
+    if(message.__contains__(comands[3])):#help comand
+        s_answer, serveraddress = clientsocket.recvfrom(2048)
+        print(s_answer.decode())
+    
     if(message.__contains__('shutdown')):
-        clientsocket.sendto('shutdown'.encode(),(servername,serverport))
+        clientsocket.sendto(message.encode(),(servername,serverport))
         break
 clientsocket.close()
 sys.exit()
--- server/server.py
+++ server/server.py
@@ -2,11 +2,15 @@ from socket import *
 import os
 import sys
 path = os.path.dirname(__file__)+"\\library\\"
+
+comands = ["list","get","put","help"]
+
 def get_files():
     files = []
     for file in os.scandir(path=path):
         files.append(file.name)
     return files
+
 def ls():
     string = ""
     file_id = 1
@@ -16,18 +20,21 @@ def ls():
     return string    
     
 server_port = 1200
+
 serversocket = socket(AF_INET, SOCK_DGRAM)
+
 server_address = ('localhost',server_port)
+
 serversocket.bind(server_address)
 print('server open on ', server_address)
 while True:
     message, client_address = serversocket.recvfrom(2048)
     print("recieved ", len(message), " bytes from ",client_address, " message: ", message.decode())
     
-    if(message.decode()=='ls'):
+    if(message.decode().__contains__(comands[0])):#list comand
        serversocket.sendto(ls().encode(),client_address)
     
-    if(message.decode().__contains__('download')):
+    if(message.decode().__contains__(comands[1])):#get comand
         file_id = int(message.decode().split()[1])
         files = get_files()
         file_name = files[file_id-1]
@@ -42,8 +49,25 @@ while True:
                 break
             else:
                 serversocket.sendto(read,client_address)
+
+    if(message.decode().__contains__(comands[2])):#put comand
+        title, client_address = serversocket.recvfrom(2048)
+        file = open(path+title.decode(),'wb')
+        while True:
+            packet, client_address = serversocket.recvfrom(2048)
+            if(packet==b''):
+                file.close()
+                break
+            else:
+                file.write(packet)
+        print("file received")
     
     if(message.decode().__contains__('shutdown')):
         serversocket.close()
         sys.exit()
-
+    
+    if(message.decode().__contains__(comands[3])):#help
+        comand_description1 = "[list] lists all avaible files" + "\n" + "[get] downloads the file selected" + "\n"
+        comand_description2 = "[put] uploads a file" + "\n"
+        comand_description = comand_description1 + comand_description2
+        serversocket.sendto(comand_description.encode(), client_address)

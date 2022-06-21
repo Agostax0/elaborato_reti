b9233f20da2cf21154d8d0a16ee26a3499dd7fd7
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-06-21 16:50:23 +0200

    now you can use numbers to refer to comands and put now displays elapsed time

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 from socket import *
 import os
-
+import time
 comands = ["list","get","put"]
 path = os.path.dirname(__file__)+"\\download\\" #if the folder doesn't exist
 if(not os.path.exists(path)):
@@ -12,11 +12,11 @@ client_socket = socket(AF_INET, SOCK_DGRAM)
 while True:
 
     message = input('input the comand: ')
-    if(message.__contains__("list")): #list comand
+    if(message.__contains__("list")or message.split()[0].__contains__("1")): #list comand
         client_socket.sendto(message.encode(),(server_name,server_port))
         s_answer, s_address = client_socket.recvfrom(2048)
         print(s_answer.decode())
-    elif(message.__contains__("get")):#get comand
+    elif(message.__contains__("get") or message.split()[0].__contains__("2")):#get comand
         client_socket.sendto(message.encode(),(server_name,server_port))
         title, s_address = client_socket.recvfrom(2048)
         if(title.decode().__contains__("File not found")):
@@ -32,22 +32,34 @@ while True:
                     file.write(packet)
         client_socket.close()
         break
-    elif(message.__contains__("put")):#put comand
-        title = message.split()[1]
+    elif(message.__contains__("put") or message.split()[0].__contains__("3")):#put comand
+        title = ""
+        message_split = message.split()
+        if(len(message_split[1:])>2): #put "test con gli spazi.txt" = ['put','test','con','gli','spazi.txt']
+            if(len(message_split[0])==1):
+                title = message[2:] #3 ["test con gli spazi.txt"]
+            else:
+                title = message[4:] #put ["test con gli spazi.txt"]
+        else:
+            title = message.split()[1] #put [spazi.txt]
+        print(title)
         try:
             file = open(path+title ,'rb')
             client_socket.sendto(message.encode(),(server_name,server_port))
-            print("sending ", title)
+            print("sending" + " \"" + title + "\"")
+            t0 = time.time()
             while True:
                 packet = file.read(2048)
                 if(packet==b''):
-                    print(title, " sent")
+                    print(title, "has been sent")
+                    print("Elapsed time = ", time.time()-t0 , " seconds")
                     client_socket.sendto(b'',(server_name,server_port))
+                    #client_socket.sendto("File Sent".encode,(server_name,server_port))
                     file.close()
                     break
                 else:
                     client_socket.sendto(packet,(server_name,server_port))
-        except:
+        except Exception:
             print("File not found")
         client_socket.close()
         break

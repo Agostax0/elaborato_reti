b8a4fed9a1bbaed0adeaaa210197c77ba62b0b58
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-06-24 12:20:40 +0200

    untested client-side "list","get" and "other" made

--- client.py
+++ client.py
@@ -19,7 +19,7 @@ while True:
     
     print(c_packet.encode())
     
-    if(c_packet.comand == "list" or c_packet == "1"): #list comand
+    if(c_packet.comand == "list" or c_packet.comand == "1"): #list comand
         client_socket.sendto(c_packet.encode(),(server_name,server_port))
         s_packet, s_address = client_socket.recvfrom(2048)
         try:
@@ -31,21 +31,33 @@ while True:
         except:
             print("There was an error")
     elif(message.__contains__("get") or message.split()[0].__contains__("2")):#get comand
-        client_socket.sendto(message.encode(),(server_name,server_port))
-        title, s_address = client_socket.recvfrom(2048)
-        if(title.decode().__contains__("File not found")):
-            print("File not found")
+        client_socket.sendto(c_packet.encode(),(server_name,server_port))
+        s_packet, s_address = client_socket.recvfrom(2048)
+        if(check_packet(s_packet)==False):
+            print("There was an error")
         else:
-            file = open(path+title.decode(),'wb')#la creazione file non crea eccezioni
-            while True:
-                packet, s_address = client_socket.recvfrom(2048)
-                if(packet==b''):
-                    file.close()
-                    break
-                else:
-                    file.write(packet)
-        client_socket.close()
-        break
+            s_packet = decode_packet(s_packet)
+            title = s_packet.subject
+            if(s_packet.ack==FILE_NOT_FOUND_ACKNOWLEDGEMENT):
+                print("File not found")
+            else:
+                file = open(path+title,'wb')#la creazione file non crea eccezioni
+                client_socket.sendto(packet(s_packet.comand,s_packet.subject,START_TRANSMISSION_ACKNOWLEDGEMENT, EMPTY_DATA).encode(),s_address)
+                while True:
+                    file_packet, s_address = client_socket.recvfrom(2048)
+                    if(check_packet(s_packet)==False):
+                        print("There was an error")
+                        file.close()
+                        break
+                    else:
+                        file_packet = decode_packet(file_packet)
+                        if(file_packet.ack == FINISHED_TRANSMISSION_ACKNOWLEDGEMENT):
+                            file.close()
+                            break
+                        else:
+                            file.write(file_packet.data)
+            client_socket.close()
+            break
     elif(message.__contains__("put") or message.split()[0].__contains__("3")):#put comand
         title = ""
         message_split = message.split()
@@ -78,8 +90,9 @@ while True:
         client_socket.close()
         break
     else:
-        client_socket.sendto(message.encode(),(server_name,server_port))
-        s_answer, s_address = client_socket.recvfrom(2048)
-        #os.system('clear')
-        #os.system('cls')
-        print(s_answer.decode())
+        client_socket.sendto(c_packet.encode(),(server_name,server_port))
+        s_packet, s_address = client_socket.recvfrom(2048)
+        if(check_packet(s_packet)):
+            print(decode_packet(s_packet).data.decode())
+        else:
+            print("data was corrupted")
